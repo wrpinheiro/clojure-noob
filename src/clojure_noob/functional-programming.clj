b34d97a367ca848a8b864ebdcef3e3964f23c772
Wellington Pinheiro <wellington.pinheiro@gmail.com>
2016-01-18 11:02:49 -0200

    Add examples from functional programming chapter.

---
+++ src/clojure_noob/functional-programming.clj
@@ -0,0 +1,74 @@
+(defn year-end-evaluation
+  []
+  (if (> (rand) 0.5)
+    "You get a raise!"
+    "Better luck next year!"))
+
+(year-end-evaluation)
+
+(def great-baby-name "Rosanthony")
+
+(let [great-baby-name "Bloodthunder"]
+  great-baby-name)
+
+(println great-baby-name)
+
+(defn sum
+  ([vals] (sum vals 0))
+  ([vals accumulating-total]
+    (if (empty? vals)
+      accumulating-total
+      (recur (rest vals) (+ (first vals) accumulating-total)))))
+
+(sum [0 1 2 3 ])
+
+(require '[clojure/string :as s])
+(defn clean
+  [text]
+  (s/replace (s/trim text) #"lol" "LOL"))
+
+((comp inc *) 2 3)
+
+(def character
+  {:name "Smooches McCutes"
+   :attributes {:intelligence 10
+                :strength 4
+                :dexterity 5}})
+(def c-int (comp :intelligence :attributes))
+(def c-str (comp :strength :attributes))
+(def c-dex (comp :dexterity :attributes))
+
+(c-int character)
+
+(defn spell-slots
+  [char]
+  (int (inc (/ (c-int char) 2))))
+
+(spell-slots character)
+
+(def spell-slots-comp (comp int inc #(/ % 2) c-int))
+
+(defn two-comp
+  [f g]
+  (fn [& args]
+    (f (apply g args))))
+
+(defn sleepy-identity
+  "Returns the given value after 1 second"
+  [x]
+  (Thread/sleep 1000)
+  x)
+(sleepy-identity "Mr. Fantastico")
+
+(def memo-sleepy-identity (memoize sleepy-identity))
+
+(memo-sleepy-identity "Mr. Fantastico")
+
+
+
+
+
+
+
+
+
